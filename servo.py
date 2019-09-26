be32dbdbef5432d0219237285552b92ea65e0edf
4J39muraoka <s16183@tokyo.kosen-ac.jp>
2019-09-26 03:35:26 +0100

    setservoの追加[s16183]

--- servo.py
+++ servo.py
@@ -6,20 +6,29 @@ GPIO.setup(2, GPIO.OUT)
 servo = GPIO.PWM(2, 50)
 servo.start(0.0)
 
-bottom = 2.5
-middle = 7.2
-top = 12.0
+BOTTOM, MIDDLE, TOP = 0.5, 1.44, 2.4
+def setservo(dig):
+    pal = 0
+    if dig == 0:
+        pal = MIDDLE
+    elif dig < 0:
+        k = (MIDDLE - BOTTOM)/90
+        pal = MIDDLE + k*dig
+    elif dig > 0:
+        k = (TOP - MIDDLE)/90
+        pal = MIDDLE + k*dig
+    
+    servo.ChangeDutyCycle(pal/20*100)
 
-for i in range(5):
-	servo.ChangeDutyCycle(bottom)
-	time.sleep(1.0)
+setservo(-90)
+time.sleep(1.0)
+setservo(-45)
+time.sleep(1.0)
+setservo(0)
+time.sleep(1.0)
+setservo(45)
+time.sleep(1.0)
+setservo(90)
+time.sleep(1.0)
 
-	servo.ChangeDutyCycle(middle)
-	time.sleep(1.0)
-
-	servo.ChangeDutyCycle(top)
-	time.sleep(1.0)
-
-
-
-
+GPIO.cleanup()
